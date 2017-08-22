1b67cd263667d1fcd5a15d31f2b9000a69cccc83
Alexander Guevara <alexgue13@gmail.com>
2017-08-21 22:49:11 -0400

    added function

---
+++ sw.js
@@ -0,0 +1,18 @@
+
+    self.addEventListener('fetch', function(event) {
+      event.respondWith(
+        caches.open('mysite').then(function(cache) {
+          return cache.match(event.request).then(function(response) {
+            var fetchPromise = fetch(event.request).then(function(networkResponse) {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            })
+
+            // Modify the response here before it goes out..
+            ...
+
+            return response || fetchPromise;
+          })
+        })
+      );
+    });
