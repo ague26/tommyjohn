c24a32fa9042c35fea473ae32b18fc7afb09e63e
Alexander Guevara <alexgue13@gmail.com>
2017-08-22 12:58:00 -0400

    last commit

--- sw.js
+++ sw.js
@@ -1,18 +1,30 @@
 
     self.addEventListener('fetch', function(event) {
       event.respondWith(
-        caches.open('mysite').then(function(cache) {
+        caches.open('parking').then(function(cache) {
           return cache.match(event.request).then(function(response) {
             var fetchPromise = fetch(event.request).then(function(networkResponse) {
               cache.put(event.request, networkResponse.clone());
               return networkResponse;
             })
 
-            // Modify the response here before it goes out..
-            ...
+           return caches.open('v1').then(function(cache)
 
             return response || fetchPromise;
           })
         })
       );
     });
+
+
+// self.addEventListener('install', function(event) {
+//  e.waitUntil(
+//    caches.open('airhorner').then(function(cache) {
+//      return cache.addAll([
+//        '/',
+//        '/index.html',
+       
+//      ]);
+//    })
+//  );
+// });
